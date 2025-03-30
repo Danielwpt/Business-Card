e6ce9bfd27ef5e9b34c919afab856f3e67218f81
Daniel <danielwong151999@gmail.com>
2025-03-30 15:45:22 +0800

    + Refactored hardcoded string to use string resource + Resized the image to fit + Refactored the details to be in the center screen

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -7,13 +7,22 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
@@ -31,22 +40,35 @@ class MainActivity : ComponentActivity() {
 
 @Preview(showSystemUi = true)
 @Composable
+fun BusinessCardMainScreen(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()) {
+
+        DisplayDetails()
+    }
+}
+
+@Composable
 fun DisplayDetails(modifier : Modifier = Modifier) {
     Column(
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
         Image(
             painter = painterResource(id = R.drawable.android_logo),
-            contentDescription = null
+            contentDescription = null,
+            contentScale = ContentScale.Fit,
+            modifier = modifier.size(150.dp)
         )
         Text(
-            text = "NAME",
+            text = stringResource(id = R.string.business_card_name),
             fontSize = 36.sp,
             fontWeight = FontWeight.ExtraBold,
             modifier = modifier
         )
         Text(
-            text = "TITLE",
+            text = stringResource(id = R.string.business_card_title),
             fontSize = 16.sp,
             fontWeight = FontWeight.Bold,
             modifier = modifier
